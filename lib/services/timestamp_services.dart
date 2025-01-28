e27483033cc01b7454eb6e1a54b7584ed2154665
tsayoo <tsaljzahidah@gmail.com>
2025-01-28 08:53:35 +0700

    finishing timestamp config data real time

--- lib/services/timestamp_services.dart
+++ lib/services/timestamp_services.dart
@@ -10,3 +10,20 @@ void setDateTime(Function(String, String, String) onDateTimeSet) {
 
   onDateTimeSet(date, time, currentDate);
 }
+
+void setAttendStatus(Function(String) onStatusSet) {
+  var dateNow = DateTime.now();
+  var hour = int.parse(DateFormat('hh').format(dateNow));
+  // int parse = ubah int jadi string
+  var minute = int.parse(DateFormat('mm').format(dateNow));
+  String attendanceStatus;
+  if (hour <= 7 || (hour == 7 && minute <= 00)) {
+    attendanceStatus = "Attend";
+  } else if (hour > 7 || (hour == 7 && minute >= 01)) {
+    attendanceStatus = "Late";
+  } else {
+    attendanceStatus = "Absent";
+  }
+
+  onStatusSet(attendanceStatus);
+}
