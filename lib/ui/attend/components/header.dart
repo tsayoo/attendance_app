0ebd82e751ce9356d6b26ce562f90b97c0ebcad0
tsayoo <tsaljzahidah@gmail.com>
2025-01-29 10:15:05 +0700

    make a header component

--- lib/ui/attend/components/header.dart
+++ lib/ui/attend/components/header.dart
@@ -4,8 +4,28 @@ Container buildHeader() {
   return Container(
     height: 50,
     decoration: BoxDecoration(
-      borderRadius: BorderRadius.only()
-      // TODO Here
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10),
+      ),
+      color: Colors.blueAccent
+    ),
+    child: Row(
+      children: [
+        SizedBox(width: 12),
+        Icon(Icons.face_retouching_natural_outlined,
+        color: Colors.white,
+        ),
+        SizedBox(width: 12),
+        Text(
+          "Please take a picture",
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+            ),
+        )
+      ],
     ),
   );
 }
