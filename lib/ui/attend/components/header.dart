b743de0bc0686f0cbee0035fa82e6c2663361fed
tsayoo <tsaljzahidah@gmail.com>
2025-01-29 10:14:44 +0700

    make a header component

---
+++ lib/ui/attend/components/header.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Container buildHeader() {
+  return Container(
+    height: 50,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.only()
+      // TODO Here
+    ),
+  );
+}
