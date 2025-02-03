cc61a52112f2fca33c2b5724f879746a08f39353
tsayoo <tsaljzahidah@gmail.com>
2025-02-03 10:36:05 +0700

    creating camera screen and kamera controller

---
+++ lib/ui/attend/camera-component/camera_controller.dart
@@ -0,0 +1,25 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraControllerComponent {
+  List<CameraDescription>? cameras;
+  CameraController? controller;
+  bool isBusy = false;
+
+  Future<void> loadCamera() async {
+    cameras = await availableCameras();
+    if (cameras != null && cameras!.isNotEmpty) {
+      controller = CameraController(cameras![0], ResolutionPreset.high);
+      // index 1 front camera index 0 back camera
+      await controller!.initialize();
+    }
+  }
+  Widget buildCameraPreview() {
+    if (controller == null || !controller!. value.isInitialized) {
+      // aksi apabila kondisi bernilai negatif
+      return const Center(child: CircularProgressIndicator());
+    } 
+    //klo aksi positif
+      return CameraPreview(controller!);
+    }
+}     
--- lib/ui/attend/camera_screen.dart
+++ lib/ui/attend/camera_screen.dart
@@ -1,3 +1,5 @@
+import 'package:attendance_app/ui/attend/camera-component/camera_controller.dart';
+import 'package:attendance_app/ui/attend/camera-component/face_detector.dart';
 import 'package:flutter/material.dart';
 
 class CameraScreen extends StatefulWidget {
@@ -7,9 +9,22 @@ class CameraScreen extends StatefulWidget {
   State<CameraScreen> createState() => _CameraScreenState();
 }
 
-class _CameraScreenState extends State<CameraScreen> {
+class _CameraScreenState extends State<CameraScreen> with TickerProviderStateMixin {
+  late CameraControllerComponent cameraControllerComponent;
+  late FaceDetectorComponent faceDetectorComponent;
+
+  @override
+  void initState() {
+    super.initState();
+    cameraControllerComponent = CameraControllerComponent();
+    faceDetectorComponent = FaceDetectorComponent();
+    cameraControllerComponent.loadCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: cameraControllerComponent.buildCameraPreview(),
+    );
   }
 }
