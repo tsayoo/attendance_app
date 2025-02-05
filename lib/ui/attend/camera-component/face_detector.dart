05fcf91d3f665a7381ca8d2c14d7e298764ada74
tsayoo <tsaljzahidah@gmail.com>
2025-02-05 09:33:38 +0700

    creating face detector function

--- lib/ui/attend/camera-component/face_detector.dart
+++ lib/ui/attend/camera-component/face_detector.dart
@@ -1,4 +1,7 @@
 
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:attendance_app/utils/google_ml_kit.dart';
 import 'package:camera/camera.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
@@ -15,6 +18,34 @@ class FaceDetectorComponent {
     final inputImage = InputImage.fromFilePath(image.path);
     final faces = await faceDetector.processImage(inputImage);
 
-    // TODO : implement Face detection.
+    for (Face face in faces) {
+      //ngasih kotak di sekitar wajahnya
+      final Rect boundingBox = face.boundingBox;
+      // Rect biar ga bisa ditipu. jd dipastiin wajahnya itu 2d bukan 1d
+      final double? verticalPosition = face.headEulerAngleY;
+      // utk ngehandle posisi wajah secara vertikal.
+      final double? horizontalPosition = face.headEulerAngleZ;
+      // utk ngehandle posisi wajah secara horizontal.
+      // knp ga pake X? karna bakalan rancu karna X itu bisa ke Y juga bisa ke Z. 
+      
+      // perkondisian apabila face landmark sudah aktif. ditandai dgn hidung mulut mata dan pipi, juga telinga.
+      final FaceLandmark? leftEar = face.landmarks[FaceLandmarkType.leftEar];
+      if (leftEar != null) {
+        final Point<int> leftEarPosition = leftEar.position;
+        // klo kupingnya kedeteksi, maka dia akan mereturn posisi kupingnya. klo ga, maka dia akan return null.
+        print("Left Ear Position: $leftEarPosition");
+}
+        // perkondisian apabila wajahnya sudah terdeteksi. ditandai dgn bibir tersenyum
+        if (face.smilingProbability != null) {
+          final double? smileProbability = face.smilingProbability;
+          print("Smile Probability: $smileProbability");
+        }
+      
+      // perkondisian apabila tracking wajah sudah aktif
+      if (face.trackingId != null) {
+        final int? trackingId = face.trackingId;
+        print("Tracking ID: $trackingId");
+      }
+    }
   }
 }
