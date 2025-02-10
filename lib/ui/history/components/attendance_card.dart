4e8e54ae9f0458a86b067f3d36f9abaf49acc05c
tsayoo <tsaljzahidah@gmail.com>
2025-02-10 09:46:54 +0700

    bikin alert dan attendance card

--- lib/ui/history/components/attendance_card.dart
+++ lib/ui/history/components/attendance_card.dart
@@ -1,12 +1,48 @@
+import 'dart:math';
+
+import 'package:attendance_app/ui/history/components/delete_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AttendanceHistoryCard extends StatelessWidget {
   final Map<String, dynamic> data;
   // map itu pasangan key Value
-  const AttendanceHistoryCard({super.key, required this.data});
+  final VoidCallback onDelete;
+
+  const AttendanceHistoryCard({super.key, required this.data, required this.onDelete});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: () {
+        DeleteDialog(
+          documentId: data["id"], 
+          dataCollection: FirebaseFirestore.instance.collection('attendance'),
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 5,
+        margin: EdgeInsets.all(10),
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
