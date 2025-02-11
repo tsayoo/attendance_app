062757b0db0214d78a39c733d862d9d940eae5c8
tsayoo <tsaljzahidah@gmail.com>
2025-02-11 07:42:25 +0700

    finishing attendance card

--- lib/ui/history/components/attendance_card.dart
+++ lib/ui/history/components/attendance_card.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:attendance_app/ui/history/components/delete_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -37,6 +36,65 @@ class AttendanceHistoryCard extends StatelessWidget {
                 decoration: BoxDecoration(
                   color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
                   borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    data["name"][0].toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 14, 
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 4,
+                          child: Text(
+                            "Student Name : ",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                            data["name"],
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14
+                            ),
+                          ),
+                        ),
+                      ]
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Attendance Status : ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14
+                          ),
+                        ),
+                        Text(
+                          data["description"],
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14
+                          ),
+                        )
+                      ],
+                    )
+                  ],
                 ),
               )
             ],
